c713fa1251b3afe9cd8fcb601ac75b373fe356c4
asim <asim71190@gmail.com>
2025-01-19 14:41:45 +0530

    temporary remove the Private/ApiKey

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,11 +1,13 @@
-import { apiKey } from "./Private/ApiKey";
+// import { apiKey } from "./Private/ApiKey";
 const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?";
 
 export default async function fetchData(city) {
   try {
-    const response = await fetch(apiUrl + "q=" + city + "&appid=" + apiKey + "&units=metric");
+    const response = await fetch(
+      apiUrl + "q=" + city + "&appid=" + apiKey + "&units=metric"
+    );
     console.log(apiUrl + "q=" + city + "&appid=" + apiKey + "&units=metric");
-    
+
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
